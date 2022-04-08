186af565cfba25fd2548d1a6337e8df273ae8eff
s00083446 <s00083446@mail.itsligo.ie>
2022-04-09 00:47:56 +0100

    Add project files.

---
+++ ToDoList_WebAPI/Models/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList_WebAPI.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ ToDoList_WebAPI/Models/ToDoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList_WebAPI.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
