662e19939ee52761920c96d56d1accf1adf8069a
s00083446 <s00083446@mail.itsligo.ie>
2022-04-09 02:01:15 +0100

    Add CRUD Functionality to the Controller

---
+++ ToDoList_WebAPI/Controllers/ToDosController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList_WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+        public ToDosController (ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ToDosController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDos()
+        {
+            return await _context.ToDos.ToListAsync();
+        }
+
+        // GET api/<ToDosController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetTodo(int id)
+        {
+            var todo = await _context.ToDos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return todo;
+
+        }
+
+        // POST api/<ToDosController>
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostTodo(ToDo todo)
+        {
+            _context.ToDos.Add(todo);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetTodo", new { id = todo.ID }, todo);
+        }
+
+        // PUT api/<ToDosController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ToDo>> PutTodo(int id, ToDo todo)
+        {
+            if (id != todo.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return todo;
+        }
+
+        // DELETE api/<ToDosController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            var todo = await _context.ToDos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            _context.ToDos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.ToDos.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ToDoList_WebAPI/Migrations/20220409002754_initial create.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Completed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
